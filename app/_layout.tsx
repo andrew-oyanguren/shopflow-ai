7956c3b7131e9ff97305645cfeda06e46a2efb24
Andrew Oyanguren <aaoyanguren@outlook.com>
2025-03-02 22:42:09 -0800

    Added env and abstracted firebase variables

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack } from "expo-router";
-import { Provider } from "react-redux";
-import store from "@/store";
+import { Stack } from 'expo-router';
+import { Provider } from 'react-redux';
+import store from '@/store';
 
 // Location to wrap Stack with Providers
 export default function RootLayout() {
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCBokn9IBjCkp1ZWfWYmVb3VHehQ7PKZrc',
-  authDomain: 'shopflow-ai.firebaseapp.com',
-  projectId: 'shopflow-ai',
-  storageBucket: 'shopflow-ai.firebasestorage.app',
-  messagingSenderId: '592933157515',
-  appId: '1:592933157515:web:eb063388bef656bbed728e',
-  measurementId: 'G-N03VN7CZ0J',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
